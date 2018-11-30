d2e3cf99e0637f88fac0d0d59c44eef43d4b538f
SkyLight <kostyan.plyusnin@yandex.ua>
2018-11-30 18:55:55 +0200

    Init commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Lab_8
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAddAnimal = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.invalidAgeExceptionBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.invalidAgeExceptionBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddAnimal
+            // 
+            this.btnAddAnimal.Location = new System.Drawing.Point(478, 10);
+            this.btnAddAnimal.Name = "btnAddAnimal";
+            this.btnAddAnimal.Size = new System.Drawing.Size(77, 35);
+            this.btnAddAnimal.TabIndex = 1;
+            this.btnAddAnimal.Text = "Добавить животное";
+            this.btnAddAnimal.UseVisualStyleBackColor = true;
+            this.btnAddAnimal.Click += new System.EventHandler(this.btnAddAnimal_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(478, 223);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(77, 26);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Закрыть";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 10);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(460, 238);
+            this.listBox1.TabIndex = 3;
+            // 
+            // invalidAgeExceptionBindingSource
+            // 
+            this.invalidAgeExceptionBindingSource.DataSource = typeof(Lab_8.InvalidAgeException);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 261);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnAddAnimal);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторна робота №8";
+            ((System.ComponentModel.ISupportInitialize)(this.invalidAgeExceptionBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAddAnimal;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.BindingSource invalidAgeExceptionBindingSource;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_8
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Animal animal = new Animal();
+            List<Animal> animals = new List<Animal>();
+            animals.Add(animal);
+            listBox1.DisplayMember = "Name";
+            listBox1.ValueMember = "Type";
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+        }
+
+        private void btnAddAnimal_Click(object sender, EventArgs e)
+        {
+            Animal animal = new Animal();
+            AnimalForm af = new AnimalForm(animal);
+            if (af.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Add(animal);
+            }
+        }
+
+        void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Animal animal = (Animal)listBox1.SelectedItem;
+            MessageBox.Show("Имя: " + animal.Name + "\n" +
+                "Вид: " + animal.Type + "\n" +
+                "Порода: " + animal.Breed + "\n" +
+                "Цвет: " + animal.Color + "\n" +
+                "Возраст: " + animal.Age + "\n" +
+                "Вес: " + animal.Weight + " кг", 
+                "Информация про животное", MessageBoxButtons.OK);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Прекратить роботу?", "Завершение работы", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
